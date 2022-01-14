e8a824cca4a05ee48b688afae1748dfea3c7e87b
Chaitanyacodes <chaitanya.chanekar2292@gmail.com>
2022-01-14 16:50:32 +0530

    style: nav-footer

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div>
-            navbar
+        <div className="nav-bar">
+            <img src="https://imgur.com/GdIUJLn.png" className="logo" />
         </div>
     )
 }
