ec571e5f0235219cd7b91b0e6ad50a5f34932ec6
Chaitanyacodes <chaitanya.chanekar2292@gmail.com>
2022-02-08 00:19:36 +0530

    refactor: icon-fix

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -9,10 +9,10 @@ function Footer() {
         <footer>
         <div className="footer-header">Lets Connect 🌏.</div>
         <ul className="list-non-bullet">
-          <li className="list-item-inline"><a href="https://github.com/ChaitanyaCodes" target="_blank" rel="noreferrer" className="link"><img src={process.env.PUBLIC_URL + '/icons/github.svg'} alt="github" /></a></li>
-          <li className="list-item-inline"><a href="https://twitter.com/ChaitanyaCodes" target="_blank" rel="noreferrer" className="link"><img src={process.env.PUBLIC_URL + '/icons/twitter.svg'} alt="twitter" /></a></li>
-          <li className="list-item-inline"><a href="https://www.linkedin.com/in/chaitanya-chanekar/" target="_blank" rel="noreferrer" className="link"><img src={process.env.PUBLIC_URL + '/icons/linkedin.svg'} alt="linkedin" /></a></li>
-          <li className="list-item-inline"><a href="https://www.instagram.com/chaitanya.22_/" target="_blank" rel="noreferrer" className="link"><img src={process.env.PUBLIC_URL + '/icons/instagram.svg'} alt="instagram" /></a></li>
+          <li className="list-item-inline"><a href="https://github.com/ChaitanyaCodes" target="_blank" rel="noreferrer" className="link"><img src="https://ik.imagekit.io/dnbsapa2jol/icons/github_MRqSCVnkX.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1644259376750" alt="github" /></a></li>
+          <li className="list-item-inline"><a href="https://twitter.com/ChaitanyaCodes" target="_blank" rel="noreferrer" className="link"><img src="https://ik.imagekit.io/dnbsapa2jol/icons/twitter_i7fviKPHq9R4.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1644259377010" alt="twitter" /></a></li>
+          <li className="list-item-inline"><a href="https://www.linkedin.com/in/chaitanya-chanekar/" target="_blank" rel="noreferrer" className="link"><img src="https://ik.imagekit.io/dnbsapa2jol/icons/linkedin_gTK_MLChtT3.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1644259376698" alt="linkedin" /></a></li>
+          <li className="list-item-inline"><a href="https://www.instagram.com/chaitanya.22_/" target="_blank" rel="noreferrer" className="link"><img src="https://ik.imagekit.io/dnbsapa2jol/icons/instagram_dllekKMQq.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1644259377707" alt="instagram" /></a></li>
         </ul>
       </footer>
 
