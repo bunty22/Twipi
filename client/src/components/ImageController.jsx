85d546359ceba1399c598f0879d6e5056b73d07b
Chaitanyacodes <chaitanya.chanekar2292@gmail.com>
2022-08-21 22:48:12 +0530

    style: border-accent

--- client/src/components/ImageController.jsx
+++ client/src/components/ImageController.jsx
@@ -21,9 +21,9 @@ function ImageController() {
 	}
 
     return (
-        <div className="config-section my-2 border border-1 rounded-lg p-3">
-			<div className="border rounded mt-2 shadow-sm">
-				<p className="p-2 mb-0">Tweet Background</p>
+        <div className="config-section my-2 border border-1 rounded-lg p-3 border-dark">
+			<div className="border rounded mt-2 shadow-sm border-dark">
+				<p className="p-2 mb-0 fw-5">Tweet Background</p>
 				<hr className="m-0" />
 				<div className="p-3">
 					<button
@@ -34,18 +34,18 @@ function ImageController() {
 					</button>
 				</div>
 			</div>
-			<div className="border rounded mt-3 shadow-sm">
+			<div className="border rounded mt-3 shadow-sm border-dark">
 				<details className="outline-none">
-					<summary className="m-2 heading">Background Image</summary>
+					<summary className="m-2 heading fw-5">Background Image</summary>
 					<hr className="mt-2 mb-0" />
 					<div>
 						< BackgroundController/>
 					</div>
 				</details>
 			</div>
-			<div className="border rounded mt-3 shadow-sm">
+			<div className="border rounded mt-3 shadow-sm border-dark">
 				<div className="p-3">
-					<label htmlFor="opacity" className="form-label d-block">
+					<label htmlFor="opacity" className="form-label d-block fw-5">
 						Opacity
 					</label>
 					<input
@@ -61,7 +61,7 @@ function ImageController() {
 				</div>
 				<hr className="m-0" />
 				<div className="p-3">
-					<label htmlFor="radius" className="form-label d-block">
+					<label htmlFor="radius" className="form-label d-block fw-5">
 						Border Radius
 					</label>
 					<input
